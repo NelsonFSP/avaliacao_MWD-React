ac42424e3ab845325b2a627cab748ebc4e6231a6
NelsonFSP <42650211+NelsonFSP@users.noreply.github.com>
2025-09-09 19:21:23 -0300

    Inclusáo de roles na criação de usuário

--- app/pages/users/create.tsx
+++ app/pages/users/create.tsx
@@ -1,8 +1,12 @@
 import { useNavigate } from "react-router"
 
 import * as userService from '../../services/user.service'
+import * as roleService from '../../services/role.service'
 import MyInput from '../../components/my.input'
 import type { User } from "~/models"
+import type { Role } from "~/models"
+import React from 'react'
+import { useState } from "react"
 
 export default function UserCreatePage() {
 
@@ -12,6 +16,9 @@ export default function UserCreatePage() {
     let username = ''
     let password = ''
     let confirmPass = ''
+
+    const [listRoles, setListRoles] = React.useState<Role[]>([])
+    const [roles, setRoles] = useState<string[]>([])
 
     function goBack() {
         navigate(-1)
@@ -35,8 +42,8 @@ export default function UserCreatePage() {
             return
         }
 
-        const user: User = { name, username, password }
-        
+        const user: User = { name, username, password, roles }
+
         userService.create(user).then(result => {
             if (result === true) goBack()
             else if (result === false) alert('Login já existe!')
@@ -44,17 +51,50 @@ export default function UserCreatePage() {
         })
     }
 
+    function fetchRoles() {
+        roleService.getList().then(list => {
+            setListRoles(list ? list : [])
+        }).catch(error => {
+            console.error(error)
+            navigate('/')
+        })
+    }
+
+    React.useEffect(() => {
+        fetchRoles()
+    }, [])
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selecionadas = Array.from(event.target.selectedOptions).map(
+            (opt) => opt.value
+        )
+        setRoles(selecionadas)
+    }
+
     return (
         <div className="page">
             <header>
                 <h3>Novo Usuário</h3>
             </header>
-            
+
             <main>
                 <MyInput title="Nome" change={value => name = value} />
                 <MyInput title="Login" change={value => username = value} />
                 <MyInput type="password" title="Senha" change={value => password = value} />
                 <MyInput type="password" title="Confirmar Senha" change={value => confirmPass = value} />
+                <select
+                    name="Roles"
+                    id="roles"
+                    onChange={handleChange
+                    }
+                    multiple
+                    value={roles}
+                >
+                    {listRoles.map(role => (
+                        <option value={role.name} >{role.name}</option>
+                    ))}
+
+                </select>
             </main>
 
             <footer>
